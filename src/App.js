7ea8bb5d9a89b843cfef60dea7243fd6cccfa7ec
gurvi20 <gurvindersingh201996@gmail.com>
2022-02-01 23:59:14 -0500

    fetching data with second API and added scroll bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const api = {
   key: "49c28aad59af4885e56a5a5151cf0c36",
@@ -8,7 +10,6 @@ const api = {
 const App = () => {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
-  const[forecast, setForecast]=useState({});
 
   const search = (evt) => {
     if (evt.key === "Enter") {
@@ -19,20 +20,36 @@ const App = () => {
           console.log(result);
         });
       setQuery("");
-      console.log(new Date( ));
+      console.log(new Date());
 
-      fetch(`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}`)
-  
-      .then((res) => res.json())
-      .then((result) => {
-        setForecast(result);
-        console.log(result );
-      });
+      // fetch(`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}`)
+
+      // .then((res) => res.json())
+      // .then((result) => {
+      //   setForecast(result);
+      //   console.log(result );
+      // });
     }
   };
 
- 
-  
+  let icon = null;
+  if (typeof weather.main != "undefined") {
+    if (weather.weather[0].main === "Clouds") {
+      icon = "fa-cloud";
+    } else if (weather.weather[0].main === "Thunderstorm") {
+      icon = "fa-bolt";
+    } else if (weather.weather[0].main === "Drizzle") {
+      icon = "fa-cloud-rain";
+    } else if (weather.weather[0].main === "Rain") {
+      icon = "fa-cloud-showers-heavy";
+    } else if (weather.weather[0].main === "Snow") {
+      icon = "fa-snowflake";
+    } else if (weather.weather[0].id >= 701 &&weather.weather[0].id <=781) {
+      icon = "fa-smog";
+    } else if (weather.weather[0].main === "Clear") {
+      icon = "fa-sun";
+    }
+  } else <div>...Loding</div>;
 
   const dateBuilder = (d) => {
     let months = [
@@ -67,7 +84,15 @@ const App = () => {
     return `${day} ${date} ${month} ${year}`;
   };
   return (
-    <div className={(typeof weather.main != "undefined")? ((weather.main.temp>16)?'app warm' :'app'):'app'}>
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
       <main>
         <div className="search-box">
           <input
@@ -88,14 +113,37 @@ const App = () => {
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
 
-            <div className="weather-box">
-              <div className="temp">{Math.round(weather.main.temp)}°c</div>
-              <div className="weather">{weather.weather[0].main}</div>
+
+<div className="weather-box">
+
+  <div className="icon"><i className={`fas ${icon} fa-4x`}></i>
+     <h1 className="weather">{weather.weather[0].main}</h1>
+     </div>
+     <div className="temp">{Math.round(weather.main.temp)}°c</div>
+<div className="min-max-temp">
+     <div className="min-temp">{weather.main.temp_max}°c Max</div>
+     <hr></hr>
+     <div className="max-temp">{weather.main.temp_min}°c Min.</div>
+     </div>
+</div>
+
+
+            {/* <div className="icon-box" >
+              <i className={`fas ${icon} fa-10x`}></i>
             </div>
+            <div className="weather-box">
+              <div className="temp">
+                <div>{Math.round(weather.main.temp)}°c</div>
+                
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div> */}
+
           </div>
-        ) :  ""
-        }
-<div class="scrolling-wrapper">
+        ) : (
+          ""
+        )}
+        {/* <div class="scrolling-wrapper">
 {typeof weather.main != "undefined" ? ( 
   typeof forecast.list != "undefined" ? (  
   forecast.list.map((element) => (
@@ -108,7 +156,7 @@ const App = () => {
           </div>
   ))):"") :  ""
         }
-        </div>
+        </div> */}
       </main>
     </div>
   );
